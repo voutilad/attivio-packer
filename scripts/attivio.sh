f5741747defa70a46ec5670a7cb99b34029ebe80
voutilad <voutilad@gmail.com>
2015-07-01 07:36:14 -0400

    added GCC for building VBox Additions

--- scripts/attivio.sh
+++ scripts/attivio.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -eux
 
-# dependencies
-#sudo yum install libstdc++.i686 -y
-#sudo yum install compat-libstdc++-33.x86_64 -y
-#yum groupinstall "Development Tools" -y
-#yum install kernel-devel -y
-
-
 # setup initial directories
 sudo mkdir /opt/attivio
 sudo chown vagrant /opt/attivio
@@ -18,3 +11,4 @@ wget http://$FILEHOST/releases/v4.3.2/x64Linux/Installer/AIE-4.3.2.99510-lin64.s
 wget http://$FILEHOST/releases/v4.3.2/x64Linux/Installer/do-not-distribute/attivio.license
 gunzip AIE-4.3.2.99510-lin64.sh.gz
 sh AIE-4.3.2.99510-lin64.sh -q -Vattivio.license.file=/opt/attivio/attivio.license
+#yum install gtk+-devel gtk2-devel
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,23 +1,15 @@
 #!/bin/bash -eux
 
-# wget install
-#sudo yum install wget -y
-
-# X11
-#yum groupinstall "X Window System" "Desktop" "Desktop Platform" -y
-#yum install gdm -y
-#yum install xauth -y
-
 ### FROM: https://github.com/gwagner/packer-centos/blob/master/provisioners/install-virtualbox-guest-additions.sh
 # Mount the disk image
 cd /tmp
-mkdir /tmp/isomount
-mount -t iso9660 -o loop /root/VBoxGuestAdditions.iso /tmp/isomount
+sudo mkdir /tmp/isomount
+sudo mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/isomount
 
 # Install the drivers
-/tmp/isomount/VBoxLinuxAdditions.run
+sudo /tmp/isomount/VBoxLinuxAdditions.run
 
 # Cleanup
-umount isomount
-rm -rf isomount /root/VBoxGuestAdditions.iso
+sudo umount isomount
+sudo rm -rf isomount /home/vagrant/VBoxGuestAdditions.iso
 ###
